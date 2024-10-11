90ff275a8aa2adfe886a33d8f17636b1d1ae3919
vietle96 <142550814+vietle96@users.noreply.github.com>
2024-10-11 02:35:46 -0700

    Add files via upload

--- start page/script.js
+++ start page/script.js
@@ -1,4 +1,8 @@
+// JavaScript File: script.js
+
+// Event listener for the "Start" button
 document.getElementById('startButton').addEventListener('click', function () {
-  document.querySelector('.center').style.display = 'none';
-  document.getElementById('nextPage').style.display = 'flex';
+  document.querySelector('.welcome-container').style.display = 'none'; // Hide the welcome message
+  document.querySelector('.center').style.display = 'none'; // Hide the button
+  document.getElementById('nextPage').style.display = 'flex'; // Show the next page content
 });
