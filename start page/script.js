17c04d389f9ad8fe3fd3f28596c51ff4ac853ecb
vietle96 <142550814+vietle96@users.noreply.github.com>
2024-10-11 00:44:16 -0700

    Add files via upload

---
+++ start page/script.js
@@ -0,0 +1,4 @@
+document.getElementById('startButton').addEventListener('click', function () {
+  document.querySelector('.center').style.display = 'none';
+  document.getElementById('nextPage').style.display = 'flex';
+});
