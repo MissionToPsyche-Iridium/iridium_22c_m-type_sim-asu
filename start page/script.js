2b6ce4dd16aaad9be76e18e61f257fd1bb118075
vietle96 <142550814+vietle96@users.noreply.github.com>
2024-11-12 01:16:39 -0700

    Add files via upload

--- start page/script.js
+++ start page/script.js
@@ -2,7 +2,7 @@
 document.getElementById('startButton').addEventListener('click', function () {
   const clickSound = document.getElementById('clickSound');
   clickSound.play(); // Play the sound on button click
-  
+
   // Fade out the welcome message and button
   document.querySelector('.welcome-container').style.opacity = '0';
   document.querySelector('.center').style.opacity = '0';
@@ -15,9 +15,7 @@ document.getElementById('startButton').addEventListener('click', function () {
     document.getElementById('nextPage').style.opacity = '1';
 
     // Display the provided image
-    const nextPageImage = document.getElementById('nextPageImage');
-    nextPageImage.src = 'psyche_asteroid.jpg'; 
-    nextPageImage.style.display = 'block';
+    window.location.href = 'asteroid.html';
   }, 500); // Matches the transition duration
 });
 
@@ -25,12 +23,20 @@ document.getElementById('startButton').addEventListener('click', function () {
 document.getElementById('creditsButton').addEventListener('click', function () {
   const clickSound = document.getElementById('clickSound');
   clickSound.play(); // Play the sound on button click
+  document.getElementById('creditsText').innerHTML = `
+        Disclaimer: This work was created in partial fulfillment of Arizona State University Capstone Course #66945.<br>
+        The work is a result of the Psyche Student Collaborations component of NASA’s Psyche Mission (<a href="https://psyche.asu.edu" target="_blank">https://psyche.asu.edu</a>).<br>
+        “Psyche: A Journey to a Metal World” [Contract number NNM16AA09C] is part of the NASA Discovery Program mission to solar system targets.<br>
+        Trade names and trademarks of ASU and NASA are used in this work for identification only.<br>
+        Their usage does not constitute an official endorsement, either expressed or implied, by Arizona State University or National Aeronautics and Space Administration.<br>
+        The content is solely the responsibility of the authors and does not necessarily represent the official views of ASU or NASA.<br><br>
+        Developers: Ash Ashour, Aureliano Ceballos, Dillon Garcia, Viet Le, and the NASA Psyche Team
+    `;
   document.getElementById('creditsModal').style.display = 'flex';
-  document.getElementById('creditsText').textContent = 'Created by Ash Ashour, Aureliano Ceballos, Dillon Garcia, Viet Le and NASA Psyche Team'; 
 });
 
 // Close modal if clicked outside of the content
-window.onclick = function(event) {
+window.onclick = function (event) {
   const modal = document.getElementById('creditsModal');
   if (event.target === modal) {
     modal.style.display = 'none';
