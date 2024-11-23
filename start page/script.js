f90b29134f59f7d1a424a670974d161a286347bd
aurelianno <aacebal2@asu.edu>
2024-11-22 17:16:08 -0700

    Add files via upload
    Added navigation bar, accessibility function, and placeholder images and facts about asteroid

--- start page/script.js
+++ start page/script.js
@@ -23,15 +23,14 @@ document.getElementById('startButton').addEventListener('click', function () {
 document.getElementById('creditsButton').addEventListener('click', function () {
   const clickSound = document.getElementById('clickSound');
   clickSound.play(); // Play the sound on button click
-  document.getElementById('creditsText').innerHTML = `
-        Disclaimer: This work was created in partial fulfillment of Arizona State University Capstone Course #66945.<br>
+  document.getElementById('creditsText').innerHTML = 
+        `Disclaimer: This work was created in partial fulfillment of Arizona State University Capstone Course #66945.<br>
         The work is a result of the Psyche Student Collaborations component of NASA’s Psyche Mission (<a href="https://psyche.asu.edu" target="_blank">https://psyche.asu.edu</a>).<br>
         “Psyche: A Journey to a Metal World” [Contract number NNM16AA09C] is part of the NASA Discovery Program mission to solar system targets.<br>
         Trade names and trademarks of ASU and NASA are used in this work for identification only.<br>
         Their usage does not constitute an official endorsement, either expressed or implied, by Arizona State University or National Aeronautics and Space Administration.<br>
         The content is solely the responsibility of the authors and does not necessarily represent the official views of ASU or NASA.<br><br>
-        Developers: Ash Ashour, Aureliano Ceballos, Dillon Garcia, Viet Le, and the NASA Psyche Team
-    `;
+        Developers: Ash Ashour, Aureliano Ceballos, Dillon Garcia, Viet Le, and the NASA Psyche Team`;
   document.getElementById('creditsModal').style.display = 'flex';
 });
 
